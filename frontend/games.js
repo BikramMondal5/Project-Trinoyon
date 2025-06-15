71df3e93a015ce3f8c6cd2d1093f81a6f692d0e3
Bikram Mondal <codesnippets45@gmail.com>
2025-06-16 02:29:40 +0530

    Add pacman

--- frontend/games.js
+++ frontend/games.js
@@ -1,38 +1,41 @@
 document.querySelector('#play-btn1').addEventListener('click',function(){
-    window.location.href="games/First-game.html";
+    window.location.href="frontend/games/First-game.html";
 });
 document.querySelector('#play-btn2').addEventListener('click',function(){
-    window.location.href="games/Second-game.html";
+    window.location.href="frontend/games/Second-game.html";
 });
 document.querySelector('#play-btn3').addEventListener('click',function(){
-    window.location.href="games/Third-game.html";
+    window.location.href="frontend/games/Third-game.html";
 });
 document.querySelector('#play-btn4').addEventListener('click',function(){
-    window.location.href="games/Fourth-game.html";
+    window.location.href="frontend/games/Fourth-game.html";
 });
 document.querySelector('#play-btn5').addEventListener('click',function(){
-    window.location.href="games/Fifth-game.html";
+    window.location.href="frontend/games/Fifth-game.html";
 });
 document.querySelector('#play-btn6').addEventListener('click',function(){
-    window.location.href="games/Sixth-game.html";
+    window.location.href="frontend/games/Sixth-game.html";
 });
 document.querySelector('#play-btn7').addEventListener('click',function(){
-    window.location.href="games/Seventh-game.html";
+    window.location.href="frontend/games/Seventh-game.html";
 });
 document.querySelector('#play-btn8').addEventListener('click',function(){
-    window.location.href="games/Eighth-game.html";
+    window.location.href="frontend/games/Eighth-game.html";
 });
 document.querySelector('#play-btn9').addEventListener('click',function(){
-    window.location.href="games/Ninth-game.html";
+    window.location.href="frontend/games/Ninth-game.html";
 });
 document.querySelector('#play-btn10').addEventListener('click',function(){
-    window.location.href="games/Tenth-game.html";
+    window.location.href="frontend/games/Tenth-game.html";
 });
 document.querySelector('#play-btn11').addEventListener('click',function(){
-    window.location.href="games/Eleventh-game.html";
+    window.location.href="frontend/games/Eleventh-game.html";
 });
 document.querySelector('#play-btn12').addEventListener('click',function(){
-    window.location.href="games/Twelveth-game.html";
+    window.location.href="frontend/games/Twelveth-game.html";
+});
+document.querySelector('#play-pacman').addEventListener('click',function(){
+    window.location.href="../pacman/index.html";
 });
 
 
